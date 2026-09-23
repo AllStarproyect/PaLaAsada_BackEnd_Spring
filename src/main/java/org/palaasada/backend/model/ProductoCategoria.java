f70481db0bf629fd65723d2d38c0afea198ee356
dagame07 <denisse.garzas07@gmail.com>
2026-09-23 17:20:21 -0600

    Changes by josea

--- src/main/java/org/palaasada/backend/model/ProductoCategoria.java
+++ src/main/java/org/palaasada/backend/model/ProductoCategoria.java
@@ -9,15 +9,30 @@ public class ProductoCategoria {
     @EmbeddedId
     private ProductoCategoriaId id;
 
+    // Constructor vacío
     public ProductoCategoria() {
     }
 
+    // Constructor lleno
+    public ProductoCategoria(ProductoCategoriaId id) {
+        this.id = id;
+    }
+
+    // Getter
     public ProductoCategoriaId getId() {
         return id;
     }
 
+    // Setter
     public void setId(ProductoCategoriaId id) {
         this.id = id;
     }
-}
 
+    // toString
+    @Override
+    public String toString() {
+        return "ProductoCategoria{" +
+                "id=" + id +
+                '}';
+    }
+}
